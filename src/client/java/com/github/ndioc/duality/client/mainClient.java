3771cdd9203f174705aab84afda6fecb7aaf4043
ndioc <wiktorsonanton@gmail.com>
2025-01-17 02:12:49 +0100

    Initial Project Push

---
+++ src/client/java/com/github/ndioc/duality/client/mainClient.java
@@ -0,0 +1,10 @@
+package com.github.ndioc.duality.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class mainClient implements ClientModInitializer {
+
+  @Override
+  public void onInitializeClient() {
+  }
+}
---
+++ src/main/java/com/github/ndioc/duality/main.java
@@ -0,0 +1,10 @@
+package com.github.ndioc.duality;
+
+import net.fabricmc.api.ModInitializer;
+
+public class main implements ModInitializer {
+
+  @Override
+  public void onInitialize() {
+  }
+}
