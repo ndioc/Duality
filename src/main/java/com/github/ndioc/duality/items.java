8cf063906163fe371b1281d50bd842dc5ac57c15
TheLoaf55 <linusiuddevalla1@gmail.com>
2025-01-19 21:46:52 +0100

    I made an orb :|

--- src/main/java/com/github/ndioc/duality/items.java
+++ src/main/java/com/github/ndioc/duality/items.java
@@ -1,8 +1,14 @@
 package com.github.ndioc.duality;
 
+import net.minecraft.item.Item;
+
+import static com.github.ndioc.duality.utilities.registeritem;
+
 public class items {
 
   public static void initialize() {
   }
 
+  public static final Item ESSENCE_ORB = registeritem("essence_orb",new Item(new Item.Settings().maxCount(1)));
+  
 }
