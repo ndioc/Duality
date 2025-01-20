13fc14060e18ed0714015082da126ecf1b7bb752
ndioc <wiktorsonanton@gmail.com>
2025-01-20 22:29:25 +0100

    added an index entry to the blockentity animatedpillar and added a getter/setter for said nbt

--- src/main/java/com/github/ndioc/duality/block/wispwood_log.java
+++ src/main/java/com/github/ndioc/duality/block/wispwood_log.java
@@ -1,7 +1,6 @@
 package com.github.ndioc.duality.block;
 
 import com.github.ndioc.duality.blockentities.animatedpillar;
-import com.mojang.serialization.MapCodec;
 import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
@@ -10,6 +9,7 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.util.math.BlockPos;
 
 public class wispwood_log extends PillarBlock implements BlockEntityProvider {
+
   public wispwood_log(Settings settings) {
     super(settings);
   }
--- src/main/java/com/github/ndioc/duality/blockentities/animatedpillar.java
+++ src/main/java/com/github/ndioc/duality/blockentities/animatedpillar.java
@@ -2,11 +2,43 @@ package com.github.ndioc.duality.blockentities;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.math.BlockPos;
 
 public class animatedpillar extends BlockEntity {
+
+  public int index = 0;
+
   public animatedpillar(BlockPos position, BlockState state) {
     super(blockentitytypes.ANIMATED_PILLAR, position, state);
   }
+
+  @Override
+  public void writeNbt(NbtCompound data) {
+    data.putInt("index", index);
+
+    super.writeNbt(data);
+  }
+
+  @Override
+  public void readNbt(NbtCompound nbt) {
+    super.readNbt(nbt);
+
+    index = nbt.getInt("number");
+  }
+
+  public void setIndex(int index) {
+    this.index = index;
+  }
+
+  public int getIndex() {
+    return index;
+  }
+
+  public void triggeranimation(){
+      
+  }
+
 }
+
+
