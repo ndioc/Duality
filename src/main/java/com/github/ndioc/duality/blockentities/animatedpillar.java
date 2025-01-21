312eb6ce07dc07f89862c11f14aa67b2d40b5c88
ndioc <wiktorsonanton@gmail.com>
2025-01-21 04:56:06 +0100

    testing

--- src/main/java/com/github/ndioc/duality/blockentities/animatedpillar.java
+++ src/main/java/com/github/ndioc/duality/blockentities/animatedpillar.java
@@ -1,22 +1,10 @@
 package com.github.ndioc.duality.blockentities;
 
-
 import com.github.ndioc.duality.block.wispwood_log;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.PillarBlock;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.state.property.Properties;
-import net.minecraft.state.property.Property;
-import net.minecraft.util.BlockRotation;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
-import net.minecraft.world.World;
-import net.minecraft.world.gen.stateprovider.BlockStateProvider;
-import net.minecraft.world.gen.stateprovider.PillarBlockStateProvider;
-
-import static net.minecraft.block.PillarBlock.AXIS;
 
 public class animatedpillar extends BlockEntity {
 
