d66cb8b2558ed969a197fa00e53c981927ec6582
TheLoaf55 <linusiuddevalla1@gmail.com>
2025-01-22 21:54:27 +0100

    Added Wisp Fruit + Textures/localization

--- src/main/java/com/github/ndioc/duality/items.java
+++ src/main/java/com/github/ndioc/duality/items.java
@@ -1,5 +1,10 @@
 package com.github.ndioc.duality;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 
 import static com.github.ndioc.duality.utilities.registeritem;
@@ -10,5 +15,15 @@ public class items {
   }
 
   public static final Item ESSENCE_ORB = registeritem("essence_orb",new Item(new Item.Settings().maxCount(1)));
-  
+
+  public static final Item WISP_FRUIT = registeritem("wisp_fruit",new Item(new FabricItemSettings()
+          .maxCount(16)
+          .food(new FoodComponent.Builder()
+                  .hunger(4)
+                  .saturationModifier(0.6f)
+                  .statusEffect(new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE,30*20, 1),1f)
+                  .build())
+
+  ));
 }
+
